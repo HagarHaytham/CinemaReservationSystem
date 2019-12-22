40e943a0fbd39352698337a7415b300a22c9a15e
hagarhaytham <hagar.bahy97@eng-st.cu.edu.eg>
2019-12-22 19:43:26 +0200

    sign up form

--- frontend/cinema/src/App.js
+++ frontend/cinema/src/App.js
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
+  import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-// import Great from './components/Signup'
+import Signup from './components/Signup'
 import Login from './components/Login'
+
 class App extends Component {
   render() {
     return (
-      <div className='rowC'>
+      // <div className='rowC'>
+      <div className='App'>
           {/* <img src={require("./images/mac.jpg")} className="background" alt="cinma" /> */}
-          <Login />
+          {/* <Login /> */}
+          <Signup></Signup>
         </div>
-    //   <div className="App">
-    //     {/* <div className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <h2>Welcome to React</h2>
-    //     </div>
-    //     <p className="App-intro">
-    //       To get started, edit <code>src/App.js</code> and save to reload.
-    //     </p> */}
-    //   {/* <Great></Great> */}
-    // <Login></Login>
-    //   </div>
     );
   }
 }
--- frontend/cinema/src/components/Signup.js
+++ frontend/cinema/src/components/Signup.js
@@ -1,13 +1,109 @@
-// import React from 'react'
-// // function Great(){
-// //     return <h1> HeLOO HAGAr</h1>
-// // }
-// const Great = () => <h1> HeLOO HAGAr</h1>
-// export default Great
-import React,{Component} from 'react'
-class Welcome extends Component{
-    render(){
-        return  <h1> HeLOO Again</h1>
+import React, { Component } from 'react'
+
+export class Signup extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             Username:'',
+             Password:'',
+             Firstname:'',
+             Lastname:'',
+             Birthday:'',
+             Email:'',
+             Type:2
+        }
+    }
+    handleUsernameChange =(event)=> {
+        this.setState({
+            Username: event.target.value
+        })
+    }
+
+    handlePasswordChange=(event)=>{
+        this.setState({
+            Password: event.target.value
+        })
+    }
+    handleFirstnameChange =(event)=>{
+        this.setState({
+            Firstname: event.target.value
+        })
+    }
+    handleLastnameChange =(event)=>{
+        this.setState({
+            Lastname: event.target.value
+        })
+    }
+    handleBirthdayChange =(event)=>{
+        this.setState({
+            Birthday: event.target.value
+        })
+    }
+    handleEmailChange = (event)=>{
+        this.setState({
+            Email: event.target.value
+        })
+    }
+    handleSubmit =(event)=>{
+        alert(`Hello ${this.state.username}`)
+        event.preventDefault()
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label>Username</label>
+                    <input required
+                    type='text' 
+                    value={this.state.username} 
+                    onChange={this.handleUsernameChange}
+                    ></input>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input required
+                    type='password'
+                    value={this.state.Password}
+                    onChange={this.handlePasswordChange}
+                    ></input>
+                </div>
+                <div>
+                    <label>FirstName</label>
+                    <input required
+                    type='text'
+                    value={this.state.Firstname}
+                    onChange={this.handleFirstnameChange}
+                    ></input>
+                </div>
+                <div>
+                    <label>LastName</label>
+                    <input required
+                    type='text'
+                    value={this.state.Lastname}
+                    onChange={this.handleLastnameChange}
+                    ></input>
+                </div>
+                <div>
+                    <label>Birthday</label>
+                    <input required
+                    type='text'
+                    value={this.state.Birthday}
+                    onChange={this.handleBirthdayChange}
+                    ></input>
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input required
+                    type='email'
+                    value={this.state.Email}
+                    onChange={this.handleEmailChange}
+                    ></input>
+                </div>
+                <button type="submit">Sign UP</button>
+            </form>
+        )
     }
 }
-export default Welcome
+
+export default Signup
