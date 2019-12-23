1ad202e0d53c307320aea6ff60e700a91264c255
hagarhaytham <hagar.bahy97@eng-st.cu.edu.eg>
2019-12-23 20:05:06 +0200

    get screens backend

--- frontend/cinema/src/components/AddMovieScreening.js
+++ frontend/cinema/src/components/AddMovieScreening.js
@@ -5,11 +5,25 @@ class AddMovieScreening extends Component {
         super(props)
     
         this.state = {
-             
+            moviename :'',
+            screens:[],
+            errorMsg:''
         }
     }
-    // {this.props.match.params.id}
-
+    // {this.props.match.params.id} ---------------------------> MovieName
+    // only executed once in the life cycle of the component
+    componentDidMount(){
+        this.setState({moviename:this.props.match.params.id})
+        axios.get('http://localhost:8089/CinemaReservationSystem/backend/getscreens.php')
+        .then(response=>{
+            console.log(response)
+            this.setState({movies:response.data})
+        })
+        .catch(error=>{
+            console.log(error)
+            this.setState({errorMsg:'Error retreiving data'})
+        })
+    }
 
     
     render() {
