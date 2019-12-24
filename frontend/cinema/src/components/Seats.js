e692eb56b3a474cbf4bc4f0129892fefa873f322
zeinabR <zeinabrabeie37@gmail.com>
2019-12-24 02:27:13 +0200

    just saving

--- frontend/cinema/src/components/Seats.js
+++ frontend/cinema/src/components/Seats.js
@@ -1,6 +1,8 @@
 import React, { Component, View } from "react";
 // import queryString from 'query-string'
-// import Seatmap from 'react-seatmap';
+import Seatmap from 'react-seatmap';
+import axios from 'axios'
+import {Link} from 'react-router-dom'
 import "../css/Login.css";
 
 class Seats extends Component {
@@ -17,17 +19,54 @@ class Seats extends Component {
   }
 
 componentDidMount() {
-  // const values = queryString.parse(this.props.location.search)
-  // console.log(values.filter) // "top"
-  // console.log(values.origin) // "im"
-  this.getScreen();
-  this.createScreen();
-          
+  this.getScreenInfo();
+  // this.getReservedSeats();
+  
+
+//  //--------------1.intializing our Array--------------//
+//   for( let i = 0 ;i<this.state.rows;i++){
+//     this.state.AllSeats.push( new  Array(this.state.cols).fill('btn-primary'));
+// }
+
+
+//     //--------------2.setting our reserved seats-------------//
+//     for(let i = 0; i<this.state.reservedSeats.length; i++){
+//         let resrvedRow = this.reservedSeats[i].row
+//         let resrvedCol = this.reservedSeats[i].col
+//         console.log(resrvedRow)
+
+//         console.log(resrvedCol)
+//         this.AllSeats[resrvedRow-1][resrvedCol-1] ='btn-danger';
+
+//         console.log(this.AllSeats)
+//     }
+
+    // this.state = {
+    //     AllSeats : this.AllSeats,
+    //     // reserved seats recieved from db
+    //     reservedSeats : this.reservedSeats,
+    //     Myseats : []
+    // }
 
 }
 
-getScreen(){
-  var apiBaseUrl = "http://localhost/backend/seats.php";
+getReservedSeats()
+{
+  var data = new FormData()
+  data.append('screenID', this.state.screenID)
+  axios.post('http://localhost/backend/seats.php', data)
+  .then(response=>{
+      console.log(response.data)
+      this.setState({seats:response.data})
+  })
+  .catch(error=>{
+      console.log(error)
+      this.setState({errorMsg:'Error retreiving data'})
+  })
+}
+
+getScreenInfo(){
+  var apiBaseUrl = "http://localhost/backend/screenInfo.php";
   var payload= 'screenID='+ this.state.screenID;
   // alert(payload);
   var xhttp = new XMLHttpRequest();
@@ -35,26 +74,21 @@ getScreen(){
   xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");     
   xhttp.onreadystatechange = function(){
     if (this.readyState === 4 && this.status === 200) {
-      // alert(this.responseText);
-      // var res = this.responseText.split(",");
+      // txt = "";
+      // var x = this.responseXML.getElementsByTagName("rows")
+      // alert(this.responseType);
+      alert(this.getResponseHeader('content-type'));
+      // for (i = 0; i< x.length; i++) {
+      //   txt += x[i].childNodes[0].nodeValue + "<br>";
+      // }
       // this.setState({
-      //   rows : res[0],
-      //   cols : res[1],
+      //   rows : this.responseText.rows,
+      //   columns : this.responseText.rows
       // });
-      // Convert data string to an object
-var data = JSON.parse(xhttp.responseText);
-// Get the first item
-var firstCell = data[0];
-
-// Loop through each cell
-data.forEach(function (cell) {
-	console.log(cell);
-});
     }
   };
   xhttp.send(payload);
 }
-
 createScreen(){
   // for( let i = 0 ;i<this.rows;i++){
   //   this.AllSeats.push( new  Array(this.cols).fill('btn-primary'));
@@ -125,14 +159,40 @@ createScreen(){
 // }
 
   render(){
+    const {seats,errorMsg} = this.state
     return (
       <div className="Seats">
         <div className="grid">
-          {/* <Group></Group> */}
-          <button className="seat"> </button>
-          <button className="seat"> </button>
+        <div className='row'>
+                    <div className='col-md-12'>
+                        <table className="table">
+                            <thead></thead>
+                            {/* -----convert each array element into ------- */}
+                            {/* ----------map throw each element in 2d array we created--------*/}
+                            <tbody>
+                                {this.state.seats.map(row => {
+                                    return (<tr key={row.Row}>
+                                        {row.map((col, ColIndex) =>
+                                            // storing our seatNumber With indxes I don not understand binding
+                                            <td key={ColIndex}><button onClick={(e) =>
+                                                this.Reserve(e, ColIndex + 1, row.Row + 1)}
+                                                /* ------check if our seat is reserved or not----- */
+                                                /* ----------TODO convert bt-danger && btn-primary into variable string */
+                                                className={"btn  btn-block " + this.state.seats[row.Row][ColIndex]} 
+                                                 value={this.index}>
+                                                {this.index++}</button></td>
+                                        )}
+                                    </tr>);
+                                }
+                                )}
+                            </tbody>
 
-          <button className="seat"> </button>
+                        </table>
+                        <div className='text-center mb-5'>
+                            <button className='btn btn-success btn-lg' onClick={ (e) => this.SubmitReservation(e)}>Get your ticket</button>
+                        </div>
+                    </div>
+                </div>
         </div>
       </div>
     );
--- frontend/cinema/src/index.js
+++ frontend/cinema/src/index.js
@@ -11,12 +11,8 @@ import './index.css';
 import AddMovie from './components/AddMovie'
 import ViewMovies from './components/ViewMovies'
 import AdminHomepage from './components/AdminHomepage'
-<<<<<<< HEAD
-// import AddMovieScreening from './components/AddMovieScreening'
-=======
-import CustomerHomePage from './components/CustomerHomePage'
+// import CustomerHomePage from './components/CustomerHomePage'
 import AddMovieScreening from './components/AddMovieScreening'
->>>>>>> 6f54daa8fd8321af32ea10b2f51163ab645c1b10
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
@@ -27,7 +23,7 @@ const routing = (
       <Route path="/login" component={Login} />
       <Route path="/signup" component={Signup} />
       <Route path='/adminhomepage' component={AdminHomepage}/>
-      <Route path='/customerhomepage' component={CustomerHomePage}/>
+      {/* <Route path='/customerhomepage' component={CustomerHomePage}/> */}
       <Route path='/addmovie' component={AddMovie}/>
       <Route path='/viewmovies' exact component={ViewMovies}/>
       {/* <Route path='/viewmovies/:id' component={AddMovieScreening} /> */}
